8b27343f5cd181c73875d0e672739e9dd5be5b64
kbhetrr <kimkh7534@naver.com>
2023-07-22 18:15:25 +0900

    KKH: 메인 화면 수정

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,14 @@ const App = () => {
     <>
       <NavBar />
       <Container>
-        <Title style={{ fontFamily: 'Pretendard' }}>안ㄴ옇사에요</Title>
-        test 입니다.
+        <Title style={{ fontFamily: 'Pretendard' }}>
+          원하는 메뉴를 클릭해주세요.
+        </Title>
+        <CardView>
+          <Card>test1</Card>
+          <Card>test2</Card>
+          <Card>test3</Card>
+        </CardView>
       </Container>
     </>
   );
@@ -28,6 +34,27 @@ const Container = styled.div`
 const Title = styled.div`
   font-family: serif;
   font-size: 28px;
+`;
+
+const CardView = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+const Card = styled.div`
+  width: calc(50% - 2rem);
+  height: 80px;
+  color: #fff;
+  margin: 1rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #123;
+  border-radius: 8px;
+
+  @media (max-width: 768px) {
+    width: 100%;
+  }
 `;
 
 export default App;
